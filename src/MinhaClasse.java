cb74c3dcc01b0f4d649a2a2ff6aa16d478a4cc9c
Douglas Olivatto <douglasolivatto@gmail.com>
2023-08-13 12:03:22 -0300

    inicio

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,8 +1,8 @@
 public class MinhaClasse {
-public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    System.out.println(" Novato no teste teste");
-    System.out.println(" Novato no PC ta osso hoje viu");
+        System.out.println(" Novato no teste teste");
+        System.out.println(" Novato no PC ta osso hoje viu");
 
-}
+    }
 }
