ad6452db3291355130d7aaffd5a6a973cea431ff
Douglas Olivatto <douglasolivatto@gmail.com>
2023-08-13 10:15:21 -0300

    novo comiit

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,7 +2,7 @@ public class MinhaClasse {
 public static void main(String[] args) {
 
     System.out.println(" Novato no teste teste");
-    System.out.println(" Novato no PC testando");
+    System.out.println(" Novato no PC ta osso hoje viu");
 
 }
 }
